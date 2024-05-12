e8627fa0beadaf79048c75645b6730ddd0904842
Merrick Clay <merrick.e.clay@gmail.com>
2024-05-12 12:47:59 -0600

    implement resize and New

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # calendar-queue
 
-A Go implementation of the calendar queue as described in Randy Brown's "Calendar Queues: A Fast O(1) Priority Queue Implementation for the Simulation Event Set Problem"
+A non-thread-safe Go implementation of the calendar queue data structure as described in Randy Brown's "Calendar Queues: A Fast O(1) Priority Queue Implementation for the Simulation Event Set Problem"
--- queue.go
+++ queue.go
@@ -1,7 +1,5 @@
 package calendarqueue
 
-// TODO: Make thread-safe
-
 type CalendarQueue[T any] struct {
 	buckets    []*event[T]
 	numBuckets int
@@ -12,17 +10,26 @@ type CalendarQueue[T any] struct {
 
 	lastPrio float64
 
-	size         int
-	topThreshold int // TODO: rename
-	botThreshold int // TODO rename
+	size               int
+	topResizeThreshold int
+	botResizeThreshold int
+
+	resizeEnabled bool
 }
 
-func (q *CalendarQueue[T]) Enqueue(entry event[T]) {
+func New[T any]() *CalendarQueue[T] {
+	q := CalendarQueue[T]{}
+	q.localInit(2, 1.0, 0.0)
+	q.resizeEnabled = true
+	return &q
+}
+
+func (q *CalendarQueue[T]) Enqueue(entry *event[T]) {
 	i := int(entry.priority/q.bucketWidth) % q.numBuckets
 
 	if q.buckets[i] == nil || q.buckets[i].priority > entry.priority {
 		entry.next = q.buckets[i]
-		q.buckets[i] = &entry
+		q.buckets[i] = entry
 	} else {
 		current := q.buckets[i]
 		for current.next != nil {
@@ -33,12 +40,12 @@ func (q *CalendarQueue[T]) Enqueue(entry event[T]) {
 			}
 		}
 		entry.next = current.next
-		current.next = &entry
+		current.next = entry
 	}
 
 	q.size++
-	if q.size > q.topThreshold {
-		// TODO: resize
+	if q.size > q.topResizeThreshold {
+		q.resize(2 * q.numBuckets)
 	}
 }
 
@@ -54,8 +61,8 @@ func (q *CalendarQueue[T]) Dequeue() *event[T] {
 			q.lastBucket = i
 			q.lastPrio = curr.priority
 			q.size--
-			if q.size < q.botThreshold {
-				// resize bucket
+			if q.size < q.botResizeThreshold {
+				q.resize(q.numBuckets / 2)
 			}
 			return curr
 		} else {
@@ -64,6 +71,7 @@ func (q *CalendarQueue[T]) Dequeue() *event[T] {
 				i = 0
 			}
 			q.bucketTop += q.bucketWidth
+
 			if i == q.lastBucket {
 				break
 			}
@@ -91,9 +99,96 @@ func (q *CalendarQueue[T]) Dequeue() *event[T] {
 	}
 
 	n := q.lastPrio / q.bucketWidth
-	q.bucketTop = (float64(n) + 1.5) * q.bucketWidth
+	q.bucketTop = (n + 1.5) * q.bucketWidth
 
 	return q.Dequeue()
+}
+
+func (q *CalendarQueue[T]) localInit(numBuckets int, bucketWidth, startPrio float64) {
+	q.buckets = make([]*event[T], numBuckets)
+	q.bucketWidth = bucketWidth
+	q.numBuckets = numBuckets
+	q.size = 0
+	q.lastPrio = startPrio
+	n := startPrio / bucketWidth
+	q.lastBucket = int(n) % numBuckets
+	q.bucketTop = (n + 1.5) * bucketWidth
+	q.botResizeThreshold = numBuckets/2 - 2
+	q.topResizeThreshold = numBuckets * 2
+}
+
+func (q *CalendarQueue[T]) resize(numBuckets int) {
+	if !q.resizeEnabled {
+		return
+	}
+	bucketWidth := q.newWidth()
+	oldBuckets := q.buckets
+	oldNumBuckets := q.numBuckets
+	q.localInit(numBuckets, bucketWidth, q.lastPrio)
+	for i := oldNumBuckets - 1; i >= 0; i-- {
+		curr := oldBuckets[i]
+		for curr != nil {
+			e := NewEvent(curr.data, curr.priority)
+			q.Enqueue(e)
+			curr = curr.next
+		}
+	}
+}
+
+func (q *CalendarQueue[T]) newWidth() float64 {
+	var numSamples int
+	if q.size < 2.0 {
+		return 1.0
+	}
+	if q.size <= 5 {
+		numSamples = q.size
+	} else {
+		numSamples = 5 + q.size/10
+	}
+	if numSamples > 25 {
+		numSamples = 25
+	}
+
+	cachedLastPrio := q.lastPrio
+	cachedLastBucket := q.lastBucket
+	cachedBucketTop := q.bucketTop
+	sampledEvents := make([]*event[T], numSamples)
+	sampledPrios := make([]float64, numSamples)
+
+	q.resizeEnabled = false
+	for i := 0; i < numSamples; i++ {
+		e := q.Dequeue()
+		sampledEvents[i] = e
+		sampledPrios[i] = e.priority
+	}
+
+	for i := numSamples - 1; i >= 0; i-- {
+		q.Enqueue(sampledEvents[i])
+	}
+
+	q.lastPrio = cachedLastPrio
+	q.lastBucket = cachedLastBucket
+	q.bucketTop = cachedBucketTop
+	q.resizeEnabled = true
+
+	separationsSum := 0.0
+	for i := 1; i < numSamples; i++ {
+		separationsSum += sampledPrios[i] - sampledPrios[i-1]
+	}
+	avgSeparation := separationsSum / float64(numSamples-1)
+
+	separationsSum = 0
+	count := 0
+	for i := 1; i < numSamples; i++ {
+		separation := sampledPrios[i] - sampledPrios[i-1]
+		if separation < avgSeparation*2 {
+			separationsSum += separation
+			count++
+		}
+	}
+	recalculatedAverage := separationsSum / float64(count)
+
+	return recalculatedAverage * 3
 }
 
 type event[T any] struct {
@@ -102,8 +197,8 @@ type event[T any] struct {
 	next     *event[T]
 }
 
-func NewEvent[T any](data T, priority float64) event[T] {
-	return event[T]{
+func NewEvent[T any](data T, priority float64) *event[T] {
+	return &event[T]{
 		data:     data,
 		priority: priority,
 		next:     nil,
